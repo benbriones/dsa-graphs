835a846c31e3060aebd7e3d12e36bad26df3d6b8
Ben Briones <benbriones50@gmail.com>
2024-04-25 11:12:22 -0700

    finished graph class and iDFS

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -21,22 +21,42 @@ class UGraphStr {
 
   /** Add node to graph. */
   addNode(node: UGraphNodeStr): void {
+    this.nodes.add(node);
   }
 
   /** Add array of nodes to graph. */
   addNodes(nodeArray: UGraphNodeStr[]): void {
+    for (let node of nodeArray) {
+      this.nodes.add(node);
+    }
   }
 
   /** Add edge between v1 and v2. */
   addEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  /** adds multiple edges */
+  addEdges(edges: UGraphNodeStr[][]) {
+    for (let edge of edges) {
+      this.addEdge(edge[0], edge[1]);
+    }
   }
 
   /** Remove edge between v1 and v2. */
   removeEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
   }
 
   /** Remove node from graph. */
   removeNode(node: UGraphNodeStr): void {
+    this.nodes.delete(node);
+    for (let neigbhor of node.adjacent) {
+      neigbhor.adjacent.delete(node);
+      node.adjacent.delete(neigbhor);
+    }
   }
 }
 
--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,3 +1,4 @@
+import { Stack, StackStr } from "../common/stack";
 import { UGraphNodeStr } from "../graph/graph";
 
 /** Return array of nodes, in DFS order (recursive version)  */
@@ -6,13 +7,32 @@ function rDfs(
     start: UGraphNodeStr,
     result: string[] = [],
     visited = new Set([start])): string[] {
+
+
   return ["todo"];
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: UGraphNodeStr): string[] {
-  return ["todo"];
+  let toVisitStack = [start];
+  let visited = new Set([start]);
+
+  const nodes = [];
+
+  while (toVisitStack.length !== 0) {
+    const curr = toVisitStack.pop();
+    nodes.push(curr!.value);
+
+    for (let neigbhor of curr!.adjacent) {
+      if(!visited.has(neigbhor)) {
+        visited.add(neigbhor);
+        toVisitStack.push(neigbhor);
+      }
+    }
+  }
+
+  return nodes;
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
